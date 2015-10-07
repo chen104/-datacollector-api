3ef82b9a9728fc7b38691c72917430719c7b05d7
Virag Kothari <virag@streamsets.com>
2015-10-07 12:23:52 -0700

    API-3 Remove Stage.isClusterMode()
    Change-Id: Id6459bcb05a6b357992b626b7336c595b024e601

--- src/main/java/com/streamsets/pipeline/api/Stage.java
+++ src/main/java/com/streamsets/pipeline/api/Stage.java
@@ -59,8 +59,6 @@ public interface Stage<C extends Stage.Context> {
 
     public boolean isPreview();
 
-    public boolean isClusterMode();
-
     public ConfigIssue createConfigIssue(String configGroup, String configName, ErrorCode errorCode, Object... args);
 
     public List<Info> getPipelineInfo();
